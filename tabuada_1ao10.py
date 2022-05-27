4746d42870ae34bc075b6d2632075fc0a929167e
pbtronco <106410460+pbtronco@users.noreply.github.com>
2022-05-27 19:45:15 -0300

    Add files via upload

---
+++ tabuada_1ao10.py
@@ -0,0 +1,6 @@
+for operador in range(1, 11):
+    print("*********************")
+    print("> Tabuada do", operador)
+    for operador2 in range(1, 11):
+        print (operador, "x", operador2, "=", operador2 * operador)
+    print("*********************")
